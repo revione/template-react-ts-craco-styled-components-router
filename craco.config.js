4fe1679e2ee1f0bc42741f6c2b27a673e9a5ce0f
Revi <oliver.ramirez@devbase.us>
2022-02-05 10:43:26 +0100

    config craco

---
+++ craco.config.js
@@ -0,0 +1,25 @@
+const CracoAlias = require("craco-alias");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: "tsconfig",
+        baseUrl: ".",
+        tsConfigPath: "./tsconfig.extra.json",
+      },
+    },
+  ],
+  babel: {
+    plugins: [
+      [
+        "babel-plugin-styled-components",
+        { displayName: process.env.NODE_ENV !== "production" },
+      ],
+    ],
+  },
+};
