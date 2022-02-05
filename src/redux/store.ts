d72e5f58520835794e06755b359fe7cc99490b2a
Revi <oliver.ramirez@devbase.us>
2022-02-05 10:55:18 +0100

    rename hooks

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,8 @@
+// libraries
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
+
+export const isDevelopment = Boolean(process.env.NODE_ENV === "development");
 
 export const store = configureStore({
   reducer: {
@@ -7,11 +10,13 @@ export const store = configureStore({
   },
 });
 
-export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
+export type Dispatch = typeof store.dispatch;
+export type Store = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
-  RootState,
+  Store,
   unknown,
   Action<string>
 >;
+
+export default store;
